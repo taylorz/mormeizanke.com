95f699ec58f809491fb9502652ef9c5e36763125
taylorz <taylor.zanke@gmail.com>
2019-10-05 15:06:41 -0400

    stuff

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Homepage.scss'
+
+class Homepage extends Component {
+  render() {
+    return (
+      <></>
+    );
+  }
+}
+
+export default Homepage;
