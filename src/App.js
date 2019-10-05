bc3183493bc65b38a1761a2f1d96f421de568bbd
taylorz <taylor.zanke@gmail.com>
2019-10-05 14:49:39 -0400

    scrub template assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, withRouter} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
