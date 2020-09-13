cdc98ead812d2d046303beee3e2b3ea1821050d2
taylorz <taylor.zanke@gmail.com>
2020-09-12 22:45:20 -0700

    photo layout

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -12,7 +12,7 @@ const About = () => (
         <p>Compelled by stories, she employs various methods and processes to bring them to life. In her essays, poetry and audio journalism she looks for the narrative and follows it to the end.</p>
         <p>Mormei acknowledges the traditional territories of the Blackfoot Confederacy, on which she lives and works, including the Tsuut’ina, the Îyâxe Nakoda Nations, The Metis Nation (region 3), and all people from the Treaty 7 region of southern Alberta.</p>
       </Grid>
-      <Grid item xs={9}>
+      <Grid item xs={8}>
         <img src={MormeiHeadshot}/>
       </Grid>
     </Grid>
