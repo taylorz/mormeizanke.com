55aeeae9f5d213717e60556ec5e13829678fdac8
Taylor Zanke <taylor.zanke@gmail.com>
2020-09-16 16:24:14 -0700

    animation refine

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -17,7 +17,7 @@ const About = () => (
     </Grid>
     <Grid container>
       <Grid item xs={12} className="bio">
-        <p>Mormei is a writer, artist and podcaster from Calgary, Alberta. She holds a Bachelor of Arts in English Literature and Creative Writing from the University of British Columbia.</p>
+        <p>Mormei is a writer, artist and podcaster from Calgary, Alberta. She holds a Bachelor of Arts in English Literature and Creative Writing from UBC.</p>
         <p>Compelled by stories, she employs various methods and processes to bring them to life. In her essays, poetry and audio journalism she looks for the narrative and follows it to the end.</p>
         <p>Mormei acknowledges the traditional territories of the Blackfoot Confederacy, on which she lives and works, including the Tsuut’ina, the Îyâxe Nakoda Nations, The Metis Nation (region 3), and all people from the Treaty 7 region of southern Alberta.</p>
       </Grid>
