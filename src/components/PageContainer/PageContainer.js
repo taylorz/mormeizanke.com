61a8f638ca868aabeef5e7ec9054aea954097174
Taylor Zanke <taylor.zanke@gmail.com>
2020-10-03 21:49:57 -0700

    random nothing

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './PageContainer.scss'
 import Grid from '@material-ui/core/Grid';
+import Footer from '../Footer/Footer'
 
 const PageContainer = ({ className, children }) => (
     <Grid container className={`page-container ${className}`}>
       <Grid item xs={12} className="content-container">
         {children}
       </Grid>
+      <Footer/>
     </Grid>
 )
 
