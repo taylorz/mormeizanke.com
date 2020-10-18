e37d19d1d105984c368376a4bb0e708be03aceeb
Taylor Zanke <taylor.zanke@gmail.com>
2020-10-17 18:02:09 -0700

    some stuff

--- src/components/WritingItem/WritingItem.js
+++ src/components/WritingItem/WritingItem.js
@@ -6,7 +6,7 @@ import Text from '../Text/Text';
 const WritingItem = ({ writing }) => (
     <a href={writing.url} target="_blank">
         <Grid container className="writing-item">
-            <Grid item xs={3} sm={1}><Text inline>{writing.year}</Text></Grid>
+            <Grid item xs={3} sm={1}><Text inline className="year">{writing.year}</Text></Grid>
             <Grid item xs={9} sm={11}>
                 <Text>{writing.title} {writing.isFavorite && <span className="favorite">*Favorite</span>}</Text>
                 <Text className="publication">{writing.publication}</Text>
