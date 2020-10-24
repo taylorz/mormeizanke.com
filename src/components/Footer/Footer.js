71549ae64b94010d5384850717066c24b9d4a4c0
Taylor Zanke <taylor.zanke@gmail.com>
2020-10-24 16:04:26 -0700

    hover animations etc

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,8 @@ import Text from '../Text/Text';
 
 const Footer = () => (
     <Grid container className="footer-container">
-        <Grid item xs={12}><Text>&copy; {(new Date().getFullYear())} Mormei Zanke</Text></Grid>
+        <Grid item xs={8}><Text>&copy; {(new Date().getFullYear())} Mormei Zanke</Text></Grid>
+        {/* <Grid item xs={4}>Newsletter</Grid> */}
     </Grid>
 )
 
