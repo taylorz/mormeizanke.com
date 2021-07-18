fe7e3dfc6b115b2791f8dbb9df3cf362f731f3ad
Taylor Zanke <taylor.zanke@gmail.com>
2021-07-17 17:00:58 -0700

    import div100vh and fix mobile scrolling issue

--- src/components/AppWrapper/AppWrapper.js
+++ src/components/AppWrapper/AppWrapper.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Div100vh from 'react-div-100vh';
 import './AppWrapper.scss'
 import Grid from '@material-ui/core/Grid';
 
 const AppWrapper = ({ children, className }) => (
+  <Div100vh>
     <Grid container className={`app-wrapper ${className && className}`}>
       <Grid item xs={12} className="wrapper-area">
         {children}
       </Grid>
     </Grid>
+  </Div100vh>
 )
 
 export default AppWrapper;
